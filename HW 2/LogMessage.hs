c18cf74d0d9c098a149884ea177815c4eb0d072f
Isaac Avram <avrisaac555@gmail.com>
2017-07-18 16:28:36 -0700

    Added insert function

--- HW 2/LogMessage.hs
+++ HW 2/LogMessage.hs
@@ -3,6 +3,8 @@ module LogAnalysis where
 
 import Log
 
+--http://www.cis.upenn.edu/~cis194/spring13/hw/02-ADTs.pdf
+
 --Parses a single line of the log file
 parseLine :: String -> LogMessage
 parseLine logString =
@@ -16,3 +18,21 @@ parseLine logString =
 parse :: String -> [LogMessage]
 parse file = [parseLine l | l <- lines file]
 
+--Inserts a LogMessage into a BST based on timestamp
+insert :: LogMessage -> MessageTree -> MessageTree
+insert insertMessage Leaf = Node Leaf insertMessage Leaf
+insert insertMessage@(LogMessage _ insertTimestamp _) (Node Leaf currentMessage@(LogMessage _ currentTimestamp _) Leaf)
+    | insertTimestamp < currentTimestamp = Node newTree currentMessage Leaf
+    | insertTimestamp > currentTimestamp = Node Leaf currentMessage newTree
+    where newTree = (Node Leaf insertMessage Leaf)
+insert insertMessage@(LogMessage _ insertTimestamp _) (Node Leaf currentMessage@(LogMessage _ currentTimestamp _) rightTree)
+    | insertTimestamp < currentTimestamp = Node newTree currentMessage rightTree
+    | insertTimestamp > currentTimestamp = insert insertMessage rightTree
+    where newTree = (Node Leaf insertMessage Leaf)
+insert insertMessage@(LogMessage _ insertTimestamp _) (Node leftTree currentMessage@(LogMessage _ currentTimestamp _) Leaf)
+    | insertTimestamp < currentTimestamp = insert insertMessage leftTree
+    | insertTimestamp > currentTimestamp = Node leftTree currentMessage newTree
+    where newTree = (Node Leaf insertMessage Leaf)
+insert insertMessage@(LogMessage _ insertTimestamp _) (Node leftTree (LogMessage _ currentTimestamp _) rightTree)
+    | insertTimestamp < currentTimestamp = insert insertMessage leftTree
+    | insertTimestamp > currentTimestamp = insert insertMessage rightTree
